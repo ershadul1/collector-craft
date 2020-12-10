4c6394c5d464e12e9a7887923e198fd06a727a0b
ershadul1 <ershadul.rayhan@gmail.com>
2020-12-10 19:55:27 +0600

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import Phaser from "phaser";
-import logoImg from "./assets/coronavirus.jpg";
-
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-const game = new Phaser.Game(config);
+import Phaser from 'phaser';
+import logoImg from './assets/coronavirus.jpg';
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('logo', logoImg);
 }
 
 function create() {
-  const logo = this.add.image(200, 150, "logo");
+  const logo = this.add.image(200, 150, 'logo');
 
   this.tweens.add({
     targets: logo,
     y: 450,
     duration: 2000,
-    ease: "Power2",
+    ease: 'Power2',
     yoyo: true,
-    loop: -1
+    loop: -1,
   });
 }
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: {
+    preload,
+    create,
+  },
+};
+
+window.game = new Phaser.Game(config);
